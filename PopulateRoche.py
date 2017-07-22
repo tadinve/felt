5077c81cbc7cd04c22a5deb4a4c11e9d770a946b
sahil <chanchadsahil@gmail.com>
2017-07-22 23:25:28 +0530

    populate script modified

--- PopulateRoche.py
+++ PopulateRoche.py
@@ -1,39 +1,45 @@
 import os, django, csv
-from roche_app.models import Roche
+from django.db import connection
 
 
 def populateRocheData():
     with open('RocheData.csv', 'r') as csvfile:
         datafile = csv.DictReader(csvfile)
         for row in datafile:
-            RocheObj = Roche()
-            rochekey = row[0]
-            process_order_number = row[1]
-            order_type = row[2]
-            material_number = row[3]
-            product_name = row[4]
-            process_order_creation_date = row[5]
-            batch_number = row[6]
-            process_order_release_date = row[7]
-            packaging_start_date = row[8]
-            packaging_end_date = row[9]
-            packaging_head_pkg_signoff = row[10]
-            bbr_start = row[11]
-            bbr_end = row[12]
-            qa_release_date = row[13]
-            product = row[14]
-            batch_status = row[15]
-            po_create_release = row[16]
-            release_to_pkg_start = row[17]
-            pkg_start_bbr_finish = row[18]
-            pkg_finish_begin = row[19]
-            brr_start_finish = row[20]
-            brr_finish_qp_release = row[21]
-            fge2e = row[22]
-            check_column =row[23]
+            i=1
+            str1 = "Insert into roche_app_roche(rochekey,process_order_number,order_type,material_number,product_name,process_order_creation_date,batch_number,process_order_release_date,packaging_start_date,packaging_end_date,packaging_head_pkg_signoff,bbr_start,bbr_end,qa_release_date,product,batch_status,po_create_release,release_to_pkg_start,pkg_start_bbr_finish,pkg_finish_begin,brr_start_finish,brr_finish_qp_release,fge2e,check_column) values('"+row['Key']+"','"+row['Process Order Number']+"','"+row['Order Type (packaging or repackaging)']+"','"+row['Material Number']+"','"+row['Product Name']+"','"+row['Process Order Creation Date']+"','"+row['Batch Number']+"','"+row['Process Order Release Date']+"','"+row['Packaging Start Date']+"','"+row['Packaging End Date']+"','"+row['Packaging Head Pkg Sigoff']+"','"+row['BRR Start']+"','"+row['BRR End']+"','"+row['QA Release Date']+"','"+row['Product']+"',"+row['Batch Status']+","+row['PO Crearte to Release']+","+row['Release to Pkg Start']+","+row['Pkg Start to Finish']+","+row['PKG Finish to BRR Begin']+","+row['BRR Start To Finish']+","+row['BRR Finish to QP Release']+","+row['FG E2E']+",'"+row['Check']+"');"
+            print(str1)
+            c = connection.cursor()
+            c.execute(str1)
+            i += 1
 
 
 
 if __name__ == '__main__':
     print("Starting to populate data")
     populateRocheData()
+
+    '''rochekey = models.CharField(max_length=125, blank=True, null=True)
+    process_order_number = models.CharField(max_length=255, blank=True, null=True)
+    order_type = models.CharField(max_length=255, blank=True, null=True)
+    material_number = models.CharField(max_length=255, blank=True, null=True)
+    product_name = models.TextField(blank=True, null=True)
+    process_order_creation_date = models.DateField(blank=True, null=True)
+    batch_number = models.CharField(max_length=255, blank=True, null=True)
+    process_order_release_date = models.DateTimeField(blank=True, null=True)
+    packaging_start_date = models.DateField(blank=True, null=True)
+    packaging_end_date = models.DateField(blank=True, null=True)
+    packaging_head_pkg_signoff = models.DateField(blank=True, null=True)
+    bbr_start = models.DateField(blank=True, null=True)
+    bbr_end = models.DateField(blank=True, null=True)
+    qa_release_date = models.DateField(blank=True, null=True)
+    product = models.CharField(max_length=255, blank=True, null=True)
+    batch_status = models.IntegerField(blank=True, null=True)
+    po_create_release = models.IntegerField(blank=True, null=True)
+    release_to_pkg_start = models.IntegerField(blank=True, null=True)
+    pkg_start_bbr_finish = models.IntegerField(blank=True, null=True)
+    pkg_finish_begin = models.IntegerField(blank=True, null=True)
+    brr_start_finish = models.IntegerField(blank=True, null=True)
+    brr_finish_qp_release = models.IntegerField(blank=True, null=True)
+    fge2e = models.IntegerField(blank=True, null=True)
+    check_column = models.CharField(max_length=15, blank=True, null=True)'''
--- roche_app/Reports.py
+++ roche_app/Reports.py
@@ -8,7 +8,7 @@ import collections
 
 
 def GetDataForChart():
-	str1 = 'select product, avg(po_create_release), avg(release_to_pkg_start), avg(pkg_start_bbr_finish), avg(pkg_finish_begin), avg(brr_start_finish), avg(brr_finish_qp_release) from roche group by product;'
+	str1 = 'select product, avg(po_create_release), avg(release_to_pkg_start), avg(pkg_start_bbr_finish), avg(pkg_finish_begin), avg(brr_start_finish), avg(brr_finish_qp_release) from roche_app_roche group by product;'
 	rows = GetDataFromDatabase(str1)
 	lis = list()
 	for row in rows:
@@ -17,7 +17,7 @@ def GetDataForChart():
 	return lis
 
 def GetAllProducts():
-	rows = GetDataFromDatabase('select Distinct(product) from roche;')
+	rows = GetDataFromDatabase('select Distinct(product) from roche_app_roche;')
 	RocheProductList = list()
 	for row in rows:
 		RocheProductList.append(row[0])
@@ -33,21 +33,21 @@ def GetDataFromDatabase(str1):
 	return lis
 
 def GetProductName(product):
-	str1 = 'select product_name from roche where product = "'+ product +'" group by product_name;'
+	str1 = 'select product_name from roche_app_roche where product = "'+ product +'" group by product_name;'
 	rows = GetDataFromDatabase(str1)
 	return rows
 
 def GetBatchNumber(product_name):
-	str1 = 'select batch_number from roche where product_name = "'+ product_name +'" group by batch_number;'
+	str1 = 'select batch_number from roche_app_roche where product_name = "'+ product_name +'" group by batch_number;'
 	#print(str1)
 	rows = GetDataFromDatabase(str1)
 	return rows
 
 def GetChartForProductName(product):
 	if product == "All" or product == "all":
-		str1 = 'select product, avg(po_create_release), avg(release_to_pkg_start), avg(pkg_start_bbr_finish), avg(pkg_finish_begin), avg(brr_start_finish), avg(brr_finish_qp_release) from roche group by product;'
+		str1 = 'select product, avg(po_create_release), avg(release_to_pkg_start), avg(pkg_start_bbr_finish), avg(pkg_finish_begin), avg(brr_start_finish), avg(brr_finish_qp_release) from roche_app_roche group by product;'
 	else:
-		str1 = 'select product_name, avg(po_create_release), avg(release_to_pkg_start), avg(pkg_start_bbr_finish), avg(pkg_finish_begin), avg(brr_start_finish), avg(brr_finish_qp_release) from roche where product = "'+ product +'" group by product_name;'
+		str1 = 'select product_name, avg(po_create_release), avg(release_to_pkg_start), avg(pkg_start_bbr_finish), avg(pkg_finish_begin), avg(brr_start_finish), avg(brr_finish_qp_release) from roche_app_roche where product = "'+ product +'" group by product_name;'
 	rows = GetDataFromDatabase(str1)
 	lis = list()
 	for row in rows:
@@ -56,9 +56,9 @@ def GetChartForProductName(product):
 
 def GetChartForBatchNumber(product,product_name):
 	if product_name == "All" or product_name == "all":
-		str1 = 'select product_name, avg(po_create_release), avg(release_to_pkg_start), avg(pkg_start_bbr_finish), avg(pkg_finish_begin), avg(brr_start_finish), avg(brr_finish_qp_release) from roche where product = "'+ product +'" group by product_name;'
+		str1 = 'select product_name, avg(po_create_release), avg(release_to_pkg_start), avg(pkg_start_bbr_finish), avg(pkg_finish_begin), avg(brr_start_finish), avg(brr_finish_qp_release) from roche_app_roche where product = "'+ product +'" group by product_name;'
 	else:
-		str1 = 'select batch_number, avg(po_create_release), avg(release_to_pkg_start), avg(pkg_start_bbr_finish), avg(pkg_finish_begin), avg(brr_start_finish), avg(brr_finish_qp_release) from roche where product_name = "'+ product_name +'" group by batch_number;'
+		str1 = 'select batch_number, avg(po_create_release), avg(release_to_pkg_start), avg(pkg_start_bbr_finish), avg(pkg_finish_begin), avg(brr_start_finish), avg(brr_finish_qp_release) from roche_app_roche where product_name = "'+ product_name +'" group by batch_number;'
 	rows = GetDataFromDatabase(str1)
 	lis = list()
 	for row in rows:
@@ -67,9 +67,9 @@ def GetChartForBatchNumber(product,product_name):
 
 def GetChartForBatchNumberFinal(product,product_name,batch_number):
 	if batch_number == "All" or product_name == "all":
-		str1 = 'select batch_number, avg(po_create_release), avg(release_to_pkg_start), avg(pkg_start_bbr_finish), avg(pkg_finish_begin), avg(brr_start_finish), avg(brr_finish_qp_release) from roche where product_name = "'+ product_name +'" group by batch_number;'
+		str1 = 'select batch_number, avg(po_create_release), avg(release_to_pkg_start), avg(pkg_start_bbr_finish), avg(pkg_finish_begin), avg(brr_start_finish), avg(brr_finish_qp_release) from roche_app_roche where product_name = "'+ product_name +'" group by batch_number;'
 	else:
-		str1 = 'select batch_number, avg(po_create_release), avg(release_to_pkg_start), avg(pkg_start_bbr_finish), avg(pkg_finish_begin), avg(brr_start_finish), avg(brr_finish_qp_release) from roche where product_name = "'+ product_name +'" and batch_number = "'+ batch_number +'";'
+		str1 = 'select batch_number, avg(po_create_release), avg(release_to_pkg_start), avg(pkg_start_bbr_finish), avg(pkg_finish_begin), avg(brr_start_finish), avg(brr_finish_qp_release) from roche_app_roche where product_name = "'+ product_name +'" and batch_number = "'+ batch_number +'";'
 		#print(str1)
 	rows = GetDataFromDatabase(str1)
 	lis = list()
