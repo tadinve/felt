c112b208c9d75a20b508d9a5be5a3fde80b4aeb5
sahil <chanchadsahil@gmail.com>
2017-08-27 10:39:36 +0530

    EC2-Dev  merge error

--- GoogleSignin/settings.py
+++ GoogleSignin/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
             'default': {
                 'ENGINE': 'django.db.backends.mysql',
                 #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-                'NAME': 'RocheDatabase',
+                'NAME': 'Roche',
                 'USER': 'root',
                 'PASSWORD': 'mysql',
                 'HOST': 'localhost',
